551964f5aeee814724031e8d23b8bcdb877edb7f
Yasmine Aït Moha <204751@stmichaelcollege.nl>
2026-05-25 15:02:58 +0200

    gewicht met vliegen berekenen

--- mag jij vliegen.py
+++ mag jij vliegen.py
@@ -1,11 +1,10 @@
-iteraties = 0
-for i in range(1, 18, 3):
-    iteraties += 1
-    print("aantal iteraties",iteraties)
-    print("loopvariabele",i)
+baggage = int(input("gewicht handbagage en kleine tas in klilogram		"))
+if baggage <12:
+    print("helemaal goed")
+elif baggage <12:
+    print("Je moet spullen uit je baggage halen of extra betalen")
+    #geef ons een 10 ik (Yasmine) heeft compensatiepunten nodig!
+else:
+    print("Nog een paar gram uit je koffer	;) ")
+    #altijd spannend of het gewicht klopt
 
-
-iteraties = 0
-while iteraties < 10:
-    iteraties += 1
-    print("aantal iteraties",iteraties)

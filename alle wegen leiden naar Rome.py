e6688e95b350196c0cf035e8145ec51573e86c6a
Luzia <204783@stmichaelcollege.nl>
2026-05-25 16:29:01 +0200

    fouten weg

--- alle wegen leiden naar Rome.py
+++ alle wegen leiden naar Rome.py
@@ -1,5 +1,6 @@
-stop = false
-while stop == false:
+vragen_en_antwoorden = []
+stop = False
+while stop == False:
     print("\n1. voeg een vraag en antwoord toe")
     print("2. geef een vraag/ antwoord terug")
     print("3. stoppen")
@@ -18,7 +19,7 @@ while stop == false:
     elif keuze == 2:
         if len(vragen_en_antwoorden) == 0:
             print("Er zijn nog geen vragen en antwoorden")
-            else:
+        else:
                 print("jouw vragen:")
                 for i in range(len(vragen_en_antwoorden)):
                     print(i, " -", vragen_en_antwoorden[i][0])
@@ -26,11 +27,11 @@ while stop == false:
                     index = int(input("Welk nummer wil je terugkrijgen?"))
                     
                     print("Vraag:", vragen_en_antwoorden[index-1][0])
-                    print("anwoord:", vragen_en_antwoorden[index-1][1]
+                    print("anwoord:", vragen_en_antwoorden[index-1][1])
                           
-            elif keuze == 3:
-                          print("Tot ziens!")
-                          stop = True
-            else:
-                print("ongeldige keuze.")
+    elif keuze == 3:
+        print("Tot ziens!")
+        stop = True
+    else:
+        print("ongeldige keuze.")
             

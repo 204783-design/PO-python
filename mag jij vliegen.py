9184af9d51a5861269158c01dd2348e035faa6e6
Yasmine Aït Moha <204751@stmichaelcollege.nl>
2026-05-25 14:42:10 +0200

    mag jij vliegen toevoegen

---
+++ mag jij vliegen.py
@@ -0,0 +1,11 @@
+iteraties = 0
+for i in range(1, 18, 3):
+    iteraties += 1
+    print("aantal iteraties",iteraties)
+    print("loopvariabele",i)
+
+
+iteraties = 0
+while iteraties < 10:
+    iteraties += 1
+    print("aantal iteraties",iteraties)

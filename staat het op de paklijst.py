ddbb57a06ebf561c8b7692d71db37cf6c3526777
Yasmine Aït Moha <204751@stmichaelcollege.nl>
2026-05-28 15:42:25 +0200

    paklijst

---
+++ staat het op de paklijst.py
@@ -0,0 +1,10 @@
+paklijst = ["zonnebrand", "zwemkleding", "broek", "zonnebril", "wandelschoenen", "oplader", "paspoort", "vliegticket", "sieraden", "kleine reis flesjes", "shampoo", "conditioner"]
+
+if input("Wat heb je mee?") in paklijst:
+    print("het zit in de paklijst")
+else:
+    print("kijk of je er ruimte voor hebt, laat het anders thuis liggen.")
+#https://www.geeksforgeeks.org/python/check-if-element-exists-in-list-in-python/
+
+           
+           
